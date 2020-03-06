e8717fa40537c64ecfd83857477f59885aa15ad8
Melvin Gonzalez <57917756+Melvin-Code@users.noreply.github.com>
2020-03-06 14:41:04 -0500

    Links are done

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <Link to='/about'>About</Link>
+                <Link to='/benefits'>Benefits</Link>
+                <Link to='/nutrition'>Nutition</Link>
+                <Link to='/recepies'>Recepies</Link>
+            </div>
+        );
+    }
+}
+
+export default NavBar;
