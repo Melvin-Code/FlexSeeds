0b124cfda3e37cc0023207777a1c415c26d1414e
Igor Zelenov <igorg.zelenov@gmail.com>
2020-03-06 14:57:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// import components
+import About from "./components/About";
+import Benefits from "./components/Benefits";
+import Footer from "./components/Footer";
+import Index from "./components/Index";
+import Navbar from "./components/Navbar";
+import Nutrition from "./components/Nutrition";
+
+// import Switch, Link, Route
+import { Switch, Link, Route } from "react-router-dom";
+
+// import axios
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Flaxseeds</h1>
     </div>
   );
 }
