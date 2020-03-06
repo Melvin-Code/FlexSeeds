ac1e519496244528ec58a0bba5c60c39ebbc4eba
Melvin Gonzalez <57917756+Melvin-Code@users.noreply.github.com>
2020-03-06 16:31:59 -0500

    done

--- src/components/AllFoods.jsx
+++ src/components/AllFoods.jsx
@@ -12,6 +12,8 @@ class AllFoods extends Component {
     
   render() {
     return (
+    <div>
+        <h1 className='title' >Super Foods</h1>
       <div className='noteCard'>
          <div><img className='bigAvo' src='/images/thought-catalog-EMX1eJ1BcgU-unsplash.jpg' /></div>
         <h3> {this.state.Avocato[0].Food_Name.toUpperCase()}</h3>
@@ -21,6 +23,19 @@ class AllFoods extends Component {
         <p> {this.state.Avocato[0].serving_size} </p>
         <h4> Calories: </h4>
         <p> {this.state.Avocato[0].calories} </p>
+      </div>
+      <div className='noteCard'>
+         <div><img className='bigAvo' src='/images\david-b-townsend-fV3zTanbO80-unsplash.jpg' /></div>
+        <h3> {this.state.salmon[0].Food_Name.toUpperCase()}</h3>
+        <h4> Description: </h4>
+        <p> {this.state.salmon[0].Description} </p>
+        <h4> Serving Size: </h4>
+        <p> {this.state.salmon[0].serving_size} </p>
+        <h4> Calories: </h4>
+        <p> {this.state.salmon[0].calories} </p>
+      </div>
+      
+         
       </div>
     );
   }
